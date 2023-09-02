77c33526e5e2ab4d2cbfdd352929c937221bb8f1
jmezzavilla <jessicamezzavilla@gmail.com>
2023-09-02 18:00:42 +0100

    pipex

--- src/execute.c
+++ src/execute.c
@@ -6,7 +6,7 @@
 /*   By: jealves- <jealves-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 16:46:09 by jealves-          #+#    #+#             */
-/*   Updated: 2023/09/02 16:46:37 by jealves-         ###   ########.fr       */
+/*   Updated: 2023/09/02 17:32:17 by jealves-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,7 +50,6 @@ void	execute_path(char **cmd, char **envp)
 		ft_clean(envp_splitted);
 		i++;
 	}
-	ft_strlen(cmd[0]);
 }
 
 void	execute(char *cmd, char **envp)
@@ -58,17 +57,12 @@ void	execute(char *cmd, char **envp)
 	char	**splitted;
 
 	splitted = ft_split(cmd, ' ');
-	if (splitted[0][0] == '/')
-	{
+	if (!splitted[0])
+		error_msg("Command not found", 127);
+	else if (splitted[0][0] == '/')
 		execve(splitted[0], splitted, envp);
-	}
 	else
-	{
-		if (!splitted[0])
-			error_msg("Command not found", 127);
-		else
-			execute_path(splitted, envp);
-	}
+		execute_path(splitted, envp);
 	ft_clean(splitted);
 	error_msg("Command not found", 127);
 }
